330bcf76d83653b5f52021710ea90b8f93e53c7f
adityarama1210 <adityarama1210@gmail.com>
2016-03-08 00:00:38 +0700

    merapihkan page about us

--- betterpet/resources/views/home/about.blade.php
+++ betterpet/resources/views/home/about.blade.php
@@ -4,28 +4,28 @@
 <div class='container-fluid'>
     <div class="row">
         <div class="about">
-
-            <h1 class="text-center"> ABOUT US</h1>
-            <hr class="line">
-            <div class="desc text-center">BetterPet is blablabla lorem ipsum blablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsum</div>
-        </div>
-
-
-        <div class="row team">
-            <div class='container-fluid'>
-                <h1 class="text-center">OUR TEAM</h1>
+            <div class="col-md-12 col-xs-12">
+                <h1 class="text-center"> ABOUT US</h1>
                 <hr class="line">
-                <div class="team">
-                    <div class="col-md-2 col-md-offset-3">
-                        <img class="img-circle" src="/PPL-A4/betterpet/public/image/test.png" width="100" height="100">
-                    </div>
-                    <div class="col-md-4">
-                        <p>MICHAEL <br>
-                            ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem psum lalalalsjajd
-                        </p>
-                    </div>  
-                </div>
+                <div class="desc text-center">BetterPet is blablabla lorem ipsum blablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsum</div>
             </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="team">
+            <h1 class="text-center">OUR TEAM</h1>
+            <hr class="line">
+            <div class="team">
+                    <div class="col-xs-3 col-xs-offset-3 col-md-3 col-md-offset-3">
+                        <img class="img-responsive img-circle" src="/PPL-A4/betterpet/public/image/test.png" width="150" height="150">
+                    </div>
+                        <div class="col-md-4 col-xs-4">
+                            <h3>Michael</h3>
+                            <p>
+                            ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem psum lalalalsjajd
+                            </p>
+                       </div> 
+               </div>
         </div>
     </div>    
     
