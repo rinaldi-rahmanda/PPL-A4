fbae8d3cfb54b4b600a99efb88c6d04602d30795
lianygustina <lianygustina@gmail.com>
2016-03-07 11:10:22 +0700

    edit

--- betterpet/resources/views/home/about.blade.php
+++ betterpet/resources/views/home/about.blade.php
@@ -1,24 +1,31 @@
 @extends('layout.template')
 
 @section('content')
-	<div class='container-fluid'>
-		 <div class="row">
-                <h1 class="text-center"> ABOUT US</h1>
-                <hr class="line">
-                <div class=" text-center">BetterPet is blablabla lorem ipsum blablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsum</div>
+<div class='container-fluid'>
+    <div class="row">
+        <div class="about">
 
+            <h1 class="text-center"> ABOUT US</h1>
+            <hr class="line">
+            <div class="desc text-center">BetterPet is blablabla lorem ipsum blablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsum</div>
+        </div>
+
+
+        <div class="row team">
+            <div class='container-fluid'>
                 <h1 class="text-center">OUR TEAM</h1>
-                <div class="table-responsive">
-                    <table class="table">
-                        <tbody>
-                            <tr>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>  
+                <hr class="line">
+                <div class="team">
+                    <div class="col-md-2 col-md-offset-3">
+                        <img class="img-circle" src="/PPL-A4/betterpet/public/image/test.png" width="100" height="100">
+                    </div>
+                    <div class="col-md-4">
+                        <p>MICHAEL <br>
+                            ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem
+                        </p>
+                    </div>  
+                </div>
             </div>
-	</div>
-@endsection
+        </div>
+    </div>
+    @endsection
