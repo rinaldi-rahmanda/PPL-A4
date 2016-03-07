e7e6c82ce0a64c1e5e75f995b6cbc0f2484325f4
adityarama1210 <adityarama1210@gmail.com>
2016-03-07 23:44:20 +0700

    Merapihkan css untuk register dan login

--- betterpet/resources/views/home/login.blade.php
+++ betterpet/resources/views/home/login.blade.php
@@ -2,20 +2,24 @@
 
 @section('content')
 	<div class='container-fluid'>
-		<form method="POST">
-			<div class="form-group">
-				<input type="text" name="email" class="form-control" placeholder="Email" required>
-			</div>
-			<div class="form-group">
-				<input type="password" name="password" class="form-control" placeholder="Password" required>
-			</div>
-			<div class="form-group">
-				<button>Sign in with Facebook</button>
-				<button>Sign in with Google</button>
-			</div>
-			<div class="form-group">
-				<button type="submit" class="register-button btn btn-primary">Submit</button>
-			</div>
-		</form>		
+		<div class="row">
+			<div class="col-xs-6 col-md-6">
+				<form method="POST">
+					<div class="form-group">
+						<input type="text" name="email" class="register-form form-control" placeholder="Email" required>
+					</div>
+					<div class="form-group">
+						<input type="password" name="password" class="register-form form-control" placeholder="Password" required>
+					</div>
+					<div class="form-group">
+						<button type="submit" class="register-button btn btn-success">Login</button>
+					</div>
+					<div class='form-group'>
+						<a href="register/button"><social class="register-button btn btn-danger">Login with Google</button></a>
+						<a href="register/social"><button class="register-button btn btn-primary">Login with Facebook</button></a>
+					</div>
+				</form>	
+			</div>	
+		</div>
 	</div>
 @endsection
--- betterpet/resources/views/home/register.blade.php
+++ betterpet/resources/views/home/register.blade.php
@@ -2,37 +2,42 @@
 
 @section('content')
 	<div class='container-fluid'>
-		<form method="POST">
-			<div class="form-group">
-				<input type="text" name="email" class="form-control" placeholder="Email" required>
+		<div class="row">
+			<div class="col-xs-6 col-md-6">
+				<form method="POST">
+					<div class="form-group">
+						<input type="text" name="email" class="register-form form-control" placeholder="Email" required>
+					</div>
+					<div class="form-group">
+						<input type="password" name="password" class="register-form form-control" placeholder="Password" required>
+					</div>
+					<div class="form-group">
+						<input type="text" name="name" class="register-form form-control" placeholder="Your Name ex: John Doe" required>
+					</div>
+					<div clas="form-group">
+						<h5 class="register-h5">Domisili</h5>
+						 <select class="register-form form-control" id="domisili">
+						    <option value="1">Jakarta Utara</option>
+						    <option value="2">Jakarta Timur</option>
+						    <option value="3">Jakarta Pusat</option>
+						    <option value="4">Jakarta Barat</option>
+						    <option value="5">Jakarta Selatan</option>
+						    <option value="6">Bogor</option>
+						    <option value="7">Depok</option>
+						    <option value="8">Tangerang</option>
+						    <option value="9">Bekasi</option>
+						 </select>
+					</div>
+					<div class="form-group">
+						<button type="submit" class="register-button btn btn-success">Submit</button>
+					</div>
+				</form>
+				<h4>Easier way to Register</h4>
+				<div class='form-group'>
+					<a href="register/button"><social class="register-button btn btn-danger">Register with Google</button></a>
+					<a href="register/social"><button class="register-button btn btn-primary">Register with Facebook</button></a>
+				</div>
 			</div>
-			<div class="form-group">
-				<input type="password" name="password" class="form-control" placeholder="Password" required>
-			</div>
-			<div class="form-group">
-				<input type="text" name="name" class="form-control" placeholder="Your Name ex: John Doe" required>
-			</div>
-			<div clas="form-group">
-				 <select class="form-control" id="domisili">
-				    <option value="1">Jakarta Utara</option>
-				    <option value="2">Jakarta Timur</option>
-				    <option value="3">Jakarta Pusat</option>
-				    <option value="4">Jakarta Barat</option>
-				    <option value="5">Jakarta Selatan</option>
-				    <option value="6">Bogor</option>
-				    <option value="7">Depok</option>
-				    <option value="8">Tangerang</option>
-				    <option value="9">Bekasi</option>
-				 </select>
-			</div>
-			<button type="submit" class="register-button btn btn-success">Submit</button>
-		</form>
-		<h4>Too lazy to fill in the form?</h4>
-		<div class='form-group'>
-			<a href="register/button"><social class="register-button btn btn-danger">Register with Google</button></a>
-		</div>
-		<div class='form-group'>
-			<a href="register/social"><button class="register-button btn btn-primary">Register with Facebook</button></a>
 		</div>
 	</div>
 @endsection
