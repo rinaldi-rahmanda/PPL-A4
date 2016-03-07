e7b1600d73e44835a8a39c05d6efb7a831a01d72
Rinaldi A. Rahmanda <rinaldiandrian@gmail.com>
2016-03-07 11:15:16 +0700

    Updated Login blade

--- betterpet/resources/views/home/login.blade.php
+++ betterpet/resources/views/home/login.blade.php
@@ -2,6 +2,20 @@
 
 @section('content')
 	<div class='container-fluid'>
-		INI LOGINPAGE
+		<form method="POST">
+			<div class="form-group">
+				<input type="text" name="email" class="form-control" placeholder="Email" required>
+			</div>
+			<div class="form-group">
+				<input type="password" name="password" class="form-control" placeholder="Password" required>
+			</div>
+			<div class="form-group">
+				<button>Sign in with Facebook</button>
+				<button>Sign in with Google</button>
+			</div>
+			<div class="form-group">
+				<button type="submit" class="register-button btn btn-primary">Submit</button>
+			</div>
+		</form>		
 	</div>
 @endsection
