e76ac465ad14dbb46c1984b56392947996c577f8
adityarama1210 <=adityarama1210@github.com>
2016-03-12 22:38:24 +0700

    adding some css

--- betterpet/resources/views/home/faq.blade.php
+++ betterpet/resources/views/home/faq.blade.php
@@ -106,19 +106,23 @@
   </style>
 
 
-<div class="jumbotron">
-</div>
 
 <!-- Container (Konten 1) -->
 <div class="container-fluid">
+	<div class='row'>
+		<div class='col-md-12 col-xs-12 col-sm-12'>
+			<div class="faq-jumbo jumbotron">
+			</div>
+		</div>
+	</div>
   <div class="row">
-    <div class="col-sm-8">
+    <div class="col-md-8 col-xs-8 col-sm-8">
       <h2>KONTEN 1</h2><br>
       <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
     </div>
-    <div class="col-sm-4">
+    <div class="col-sm-4 col-xs-12 col-sm-12">
     	<div class="poslog">
      	 <span class="glyphicon glyphicon-signal logo"></span>
     	</div>
