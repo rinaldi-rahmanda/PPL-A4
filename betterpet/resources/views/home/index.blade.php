f6403571cbbd4596c1dc293f99f01783aeb5d1b8
adityarama1210 <adityarama1210@gmail.com>
2016-03-14 09:50:32 +0700

    home dropdown fixed

--- betterpet/resources/views/home/index.blade.php
+++ betterpet/resources/views/home/index.blade.php
@@ -46,11 +46,6 @@
 
 <!-- /.container -->
 
-<!-- jQuery -->
-<script src="js/jquery.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
 
 <!-- Script to Activate the Carousel -->
 <script>
