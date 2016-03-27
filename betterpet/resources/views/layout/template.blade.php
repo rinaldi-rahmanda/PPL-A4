164ccf5bf147a3a25f76f24ab1a788dd3b259a6d
adityarama1210 <adityarama1210@gmail.com>
2016-03-27 13:25:05 +0700

    add more

--- betterpet/resources/views/layout/template.blade.php
+++ betterpet/resources/views/layout/template.blade.php
@@ -37,7 +37,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>                        
                     </button>
-                    <a href="/PPL-A4/betterpet/public/"><img src="/PPL-A4/betterpet/public/image/logo.png" width="150" height="60"></a>
+                    <a href="{{URL::to('/')}}"><img src="/PPL-A4/betterpet/public/image/logo.png" width="150" height="60"></a>
                 </div>
                 <div class="collapse navbar-collapse" id="myNavbar">
                     <ul class="nav navbar-nav navbar-right">
