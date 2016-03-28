5963741feb7a8e62156bdf2f32bf4ad1734fa7e0
adityarama1210 <=adityarama1210@github.com>
2016-03-28 14:00:11 +0700

    main carousel fixed

--- betterpet/resources/views/home/index.blade.php
+++ betterpet/resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <!-- Full Page Image Background Carousel Header -->
 
-<div id="myCarousel" class="carousel slide">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
