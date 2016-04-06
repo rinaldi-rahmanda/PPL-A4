72fbd044ad8c92d971c33a259fb4e7a71be21866
adityarama1210 <=adityarama1210@github.com>
2016-04-06 10:27:05 +0700

    footer updated

--- betterpet/resources/views/home/news.blade.php
+++ betterpet/resources/views/home/news.blade.php
@@ -6,36 +6,33 @@
 		<div class="row">
             <h3 class="text-center"> Read our latest news</h3>
             <hr class="line">
-			<div class="col-sm-4 col-xs-4 col-md-4">
+			<div class="col-sm-12 col-xs-12 col-md-12">
 				<div class="news-thumbnail thumbnail">
-				  <img src="image/faiz.jpg" class='img-responsive' >
 				  <div class="caption">
 					<h3>Judul News</h3>
 					<p>Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala</p>
-					<p><a href="{{URL::to('/news1')}}" class="btn btn-default" role="button">Read More</a></p>
+					<p><a href="{{URL::to('/news/1')}}" class="btn btn-default" role="button">Read More</a></p>
 				  </div>
 				</div>
-		  </div>
-		  <div class="col-sm-4 col-xs-4 col-md-4">
-				<div class=" news-thumbnail thumbnail">
-					<img src="image/faiz.jpg" class='img-responsive' >
-				  <div class="caption">
-					<h3>Judul News</h3>
-					<p>Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala</p>
-					<p><a href="#" class="btn btn-default" role="button">Read More</a></p>
-				  </div>
-				</div>
-		  </div>
-		  <div class="col-sm-4 col-xs-4 col-md-4">
+			</div>
+			<div class="col-sm-12 col-xs-12 col-md-12">
 				<div class="news-thumbnail thumbnail">
-				  <img src="image/faiz.jpg" class='img-responsive' >
 				  <div class="caption">
 					<h3>Judul News</h3>
 					<p>Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala</p>
-					<p><a href="#" class="btn btn-default" role="button">Read More</a></p>
+					<p><a href="{{URL::to('/news/1')}}" class="btn btn-default" role="button">Read More</a></p>
 				  </div>
 				</div>
-		  </div>
+			</div>
+			<div class="col-sm-12 col-xs-12 col-md-12">
+				<div class="news-thumbnail thumbnail">
+				  <div class="caption">
+					<h3>Judul News</h3>
+					<p>Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala</p>
+					<p><a href="{{URL::to('/news/1')}}" class="btn btn-default" role="button">Read More</a></p>
+				  </div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
