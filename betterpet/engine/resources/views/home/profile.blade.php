d2ef099735a21593704f9800887999e66fdd4216
adityarama1210 <=adityarama1210@github.com>
2016-04-07 16:05:18 +0700

    edit profile

--- betterpet/engine/resources/views/home/profile.blade.php
+++ betterpet/engine/resources/views/home/profile.blade.php
@@ -16,23 +16,28 @@
             </div>
         </div>
         <div class="col-md-offset-2 col-md-2">
-
             <img src="http://placehold.it/200x200" alt="">
             <div class="ava-caption">
             	Edit your avatar
             	<img src="image/pen.png" id="upfile1" style="cursor:pointer" width="20" height="20" style="float:right;"/>
-		<input type="file" id="file1"  name="file1" style="display:none" />
+		<input form="profile-form" type="file" id="file1"  name="profilePicture" style="display:none" />
             </div>
         </div>
         <div class="col-md-offset-1 col-md-5 col-xs-6">
 
             
 
-            <form class="form-horizontal" method="POST">
+            <form id="profile-form" class="form-horizontal" method="POST">
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-3 control-label">Name</label>
                     <div class="col-sm-9">
-                        <input type="text" class="register-form profile-form form-control" placeholder="Your name">
+                        <input type="text" class="register-form profile-form form-control" name="name" value="{{$user->name}}">
+                    </div>
+                </div>
+				<div class="form-group">
+                    <label for="inputAddressl3" class="col-sm-3 control-label">Address</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="register-form profile-form form-control" name="address" value="{{$address}}">
                     </div>
                 </div>
                 <div class="form-group">
@@ -81,14 +86,20 @@
                     
                 </div>
                 <div class="form-group">
-                    <label for="inputPassword3" class="col-sm-3 control-label">Phone number</label>
+                    <label for="inputPhone" class="col-sm-3 control-label">Phone number</label>
                     <div class="col-sm-9">
-                        <input type="text" class="register-form profile-form form-control" placeholder="Phone number">
+                        <input type="text" class="register-form profile-form form-control" name="phone" value="{{$phone}}">
+                    </div>
+                </div>
+				<div class="form-group">
+                    <label for="inputDesc" class="col-sm-3 control-label">Description</label>
+                    <div class="col-sm-9">
+						<textarea class="register-form profile-form form-control" name="description" value="{{$description}}"></textarea>
                     </div>
                 </div>
                 <div class="form-group">
-                        <button type="submit" class="register-button btn btn-success">Save</button>
-                    </div>
+                    <button type="submit" class="register-button btn btn-success">Update Profile</button>
+                </div>
             </form>
         </div>
     </div>
