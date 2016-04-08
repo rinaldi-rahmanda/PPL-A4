47e9d68991c2a60acd8ad749e984e0c121baed74
lianygustina <lianygustina@gmail.com>
2016-04-08 15:48:07 +0700

    change color background and fix news thumbnail

--- betterpet/engine/resources/views/home/news.blade.php
+++ betterpet/engine/resources/views/home/news.blade.php
@@ -2,38 +2,41 @@
 
 @section('content')
 <div class='container-fluid page-wrap' style='margin-top:100px;' >
-	<div class='news'>
-		<div class="row">
+    <div class='news'>
+        <div class="row">
             <h3 class="text-center"> Read our latest news</h3>
             <hr class="line">
-			<div class="col-sm-12 col-xs-12 col-md-12">
-				<div class="news-thumbnail thumbnail">
-				  <div class="caption">
-					<h3>Judul News</h3>
-					<p>Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala</p>
-					<p><a href="{{URL::to('/news/1')}}" class="btn btn-default" role="button">Read More</a></p>
-				  </div>
-				</div>
-			</div>
-			<div class="col-sm-12 col-xs-12 col-md-12">
-				<div class="news-thumbnail thumbnail">
-				  <div class="caption">
-					<h3>Judul News</h3>
-					<p>Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala</p>
-					<p><a href="{{URL::to('/news/1')}}" class="btn btn-default" role="button">Read More</a></p>
-				  </div>
-				</div>
-			</div>
-			<div class="col-sm-12 col-xs-12 col-md-12">
-				<div class="news-thumbnail thumbnail">
-				  <div class="caption">
-					<h3>Judul News</h3>
-					<p>Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala</p>
-					<p><a href="{{URL::to('/news/1')}}" class="btn btn-default" role="button">Read More</a></p>
-				  </div>
-				</div>
-			</div>
-		</div>
-	</div>
+            <div class="col-md-offset-1 col-sm-10 col-xs-10 col-md-10 news-thumbnail">
+                <div class="col-md-4" style="margin-left:2%;margin-top:2%;margin-bottom:2%;"><img src="{{URL::to('/image/samplex.jpg')}}" width="200" height="250"> </div>
+                <div class="col-md-7">
+                    <div class="caption">
+                        <h3>Judul News</h3>
+                        <p>Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala</p>
+                        <p><a href="{{URL::to('/news/1')}}" class="btn btn-default" role="button">Read More</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-offset-1 col-sm-10 col-xs-10 col-md-10 news-thumbnail">
+                <div class="col-md-4" style="margin-left:2%;margin-top:2%;margin-bottom:2%;"><img src="{{URL::to('/image/samplex.jpg')}}" width="200" height="250"> </div>
+                <div class="col-md-7">
+                    <div class="caption">
+                        <h3>Judul News</h3>
+                        <p>Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala</p>
+                        <p><a href="{{URL::to('/news/1')}}" class="btn btn-default" role="button">Read More</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-offset-1 col-sm-10 col-xs-10 col-md-10 news-thumbnail">
+                <div class="col-md-4" style="margin-left:2%;margin-top:2%;margin-bottom:2%;"><img src="{{URL::to('/image/samplex.jpg')}}" width="200" height="250"> </div>
+                <div class="col-md-7">
+                    <div class="caption">
+                        <h3>Judul News</h3>
+                        <p>Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala Lorem ipsum lalala</p>
+                        <p><a href="{{URL::to('/news/1')}}" class="btn btn-default" role="button">Read More</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
--- betterpet/engine/resources/views/home/viewProfile.blade.php
+++ betterpet/engine/resources/views/home/viewProfile.blade.php
@@ -1,23 +1,29 @@
 @extends('layout.template-about')
 
 @section('content')
+<style>
+    body {
+        background-color:#dfeef7;        
+    }
+</style>
 
-<div class='container-fluid'>
+<div class='container-fluid page-wrap'>
     <div class="row">
-
-        <div class="col-md-offset-2 col-md-2" style="margin-top:10%;margin-bottom:2%;">
-            <img src="http://placehold.it/200x200" alt="">
-        </div>
-        <div class="col-md-offset-1 col-md-5" style="margin-top:8%;margin-bottom:2%;">
-            <h1 >Hello, I am {{ $user->name }}!</h1>
-            <ul style="padding: 0;"><li style="display:inline;">{{ $domicile }}</li><li style="display:inline;padding-left:10px;">&#8226 Member since</li></ul>
-            <div class="panel panel-default">
-                <div class="panel-heading">About Me</div>
-                <div class="panel-body">
-                    <p>{{$description}}</p>
-                </div>
-            </div>      
-        </div>
+        <div class="col-md-offset-2 col-md-8 col-sm-8" style="background-color:white; min-height: 100%; margin-top:8%;">
+            <div class="col-md-2 col-sm-2" style="margin-top:5%;margin-bottom:2%;padding-left:3%;">
+                <img src="http://placehold.it/200x200" alt="">
+            </div>
+            <div class="col-md-offset-1 col-sm-offset-1 col-md-5 col-sm-5" style="margin-top:3%;margin-bottom:2%;padding-left:4%;">
+                <h1 >Hello, I am {{ $user->name }}!</h1>
+                <ul style="padding: 0;"><li style="display:inline;">{{ $domicile }}</li><li style="display:inline;padding-left:10px;">&#8226 Member since</li></ul>
+                <div class="panel panel-default">
+                    <div class="panel-heading">About Me</div>
+                    <div class="panel-body">
+                        <p>{{$description}}</p>
+                    </div>
+                </div>      
+            </div>
+        </div>      
     </div>
 </div>
 @endsection
