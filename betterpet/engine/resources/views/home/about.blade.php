60b6ed7703de52979e9f8f0e13f515d99da60991
faizkautsarr <faizkautsarr@gmail.com>
2016-04-09 14:20:38 +0700

    isi konten

--- betterpet/engine/resources/views/home/about.blade.php
+++ betterpet/engine/resources/views/home/about.blade.php
@@ -49,7 +49,7 @@
                     <div class="col-md-4 col-xs-4">
                         <h3>Aditya Rama</h3>
                         <blockquote><i>
-                            "Growing together with a pet is wonderful, and i hope i'm not the only one who feel it"
+                            "Growing together with a pet is wonderful, and i hope i'm not the only one who feel it."
                             </i>
                         </blockquote>
                     </div> 
@@ -59,7 +59,7 @@
                     <div class="col-xs-3 col-xs-offset-3 col-md-4 col-md-offset-3">
                         <h3>Faiz Kautsar</h3>
                         <blockquote><i>
-                            ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem ipsumblablabla lorem psum lalalalsjajd
+                           "I really love cat, and i think betterpet will really helpful for pet lovers."
                             </i>
                         </blockquote>
                     </div>
