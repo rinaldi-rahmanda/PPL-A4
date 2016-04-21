c679cfd1e4f9592c2beaaef5dc3395a1f61cd6e0
saprikan <tommy.saprikan@gmail.com>
2016-04-21 13:15:51 +0700

    Admin

--- betterpet/engine/resources/views/admin/index.blade.php
+++ betterpet/engine/resources/views/admin/index.blade.php
@@ -7,11 +7,15 @@
   body {
       position: relative; 
   }
-  #section1 {padding-top:50px;height:500px;color: #fff; background-color: #1E88E5;}
-  #section2 {padding-top:50px;height:500px;color: #fff; background-color: #673ab7;}
-  #section3 {padding-top:50px;height:500px;color: #fff; background-color: #ff9800;}
+  #section1 {padding-top:50px;height:500px;color: #08466A; background-color: #5AC2FF;}
+  #section2 {padding-top:50px;height:500px;color: #08466A; background-color: #f6f6f6;}
+  #section3 {padding-top:50px;height:500px;color: #fff; background-color: #009688;}
   #section41 {padding-top:50px;height:500px;color: #fff; background-color: #00bcd4;}
   #section42 {padding-top:50px;height:500px;color: #fff; background-color: #009688;}
+  .navbar-default .navbar-nav > li > a:hover, .navbar-default .navbar-nav > li > a:focus {
+   background-color: #5AC2FF;
+}
+  
   </style>
 </head>
 <body data-spy="scroll" data-target=".navbar" data-offset="50">
@@ -19,28 +23,33 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">BETTERPET</a>
+      <a href="{{URL::to('/')}}"><img src="{{URL::to('/image/logo.png')}}" width="150" height="60"></a>
     </div>
     <div>
       <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav">
+          <li><a href="#section1">Shelter</a></li>
           <li><a href="#section2">Section 2</a></li>
           <li><a href="#section3">Section 3</a></li>
+          <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">News<span class="caret"></span></a>
             <ul class="dropdown-menu">
+              <li><a href="#section41">List News</a></li>
+              <li><a href="#section42">Create news</a></li>
             </ul>
           </li>
         </ul>
+        <ul class="nav navbar-nav navbar-right">
+        	<li><a> Admin</a></li>
+        	<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Out</a></li>
+     	</ul>
+
       </div>
     </div>
   </div>
 </nav>    
 
 <div id="section1" class="container-fluid">
+  <h1>Shelter</h1>
   <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
   <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
 </div>
@@ -55,10 +64,12 @@
   <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
 </div>
 <div id="section41" class="container-fluid">
+  <h1>List News</h1>
   <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
   <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
 </div>
 <div id="section42" class="container-fluid">
+  <h1>Create News</h1>
   <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
   <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
 </div>
