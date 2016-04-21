ea86f2aa275632e36d69c4be4bc32f7f50b00ffd
saprikan <tommy.saprikan@gmail.com>
2016-04-21 13:51:13 +0700

    Admin

--- betterpet/engine/resources/views/admin/index.blade.php
+++ betterpet/engine/resources/views/admin/index.blade.php
@@ -7,10 +7,10 @@
   body {
       position: relative; 
   }
-  #section1 {padding-top:50px;height:500px;color: #08466A; background-color: #5AC2FF;}
-  #section2 {padding-top:50px;height:500px;color: #08466A; background-color: #f6f6f6;}
+  #section1 {padding-top:50px;height:500px;color: #101010; background-color: #5AC2FF;}
+  #section2 {padding-top:50px;height:500px;color: #101010; background-color: #f6f6f6;}
   #section3 {padding-top:50px;height:500px;color: #fff; background-color: #009688;}
-  #section41 {padding-top:50px;height:500px;color: #fff; background-color: #00bcd4;}
+  #section41 {padding-top:50px;height:500px;color: #fff; background-color: #f6f6f6;}
   #section42 {padding-top:50px;height:500px;color: #fff; background-color: #009688;}
   .navbar-default .navbar-nav > li > a:hover, .navbar-default .navbar-nav > li > a:focus {
    background-color: #5AC2FF;
@@ -29,12 +29,12 @@
       <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav">
           <li><a href="#section1">Shelter</a></li>
-          <li><a href="#section2">Section 2</a></li>
+          <li><a href="#section2">Users</a></li>
           <li><a href="#section3">Section 3</a></li>
           <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">News<span class="caret"></span></a>
             <ul class="dropdown-menu">
               <li><a href="#section41">List News</a></li>
-              <li><a href="#section42">Create news</a></li>
+              <li><a href="#section42">Create News</a></li>
             </ul>
           </li>
         </ul>
@@ -49,30 +49,28 @@
 </nav>    
 
 <div id="section1" class="container-fluid">
-  <h1>Shelter</h1>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <h1 class="text-center">SHELTER</h1>
+  <p>Dafatar penitipan hewan</p>
+ 
 </div>
 <div id="section2" class="container-fluid">
-  <h1>Section 2</h1>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <h1 class="text-center">USERS</h1>
+  <p>Daftar User</p>
+  
 </div>
 <div id="section3" class="container-fluid">
-  <h1>Section 3</h1>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-</div>
+  <h1 class="text-center">Section 3</h1>
+  <p>Datar -----------</p>
+  </div>
 <div id="section41" class="container-fluid">
-  <h1>List News</h1>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <h1 class="text-center">LIST NEWS</h1>
+  <p>List semua news</p>
+  
 </div>
 <div id="section42" class="container-fluid">
-  <h1>Create News</h1>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-</div>
+  <h1 class="text-center">CREATE NEWS</h1>
+  <p>Membuat news baru</p>
+  </div>
 
 </body>
 @endsection
