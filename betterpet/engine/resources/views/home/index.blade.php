d42ce6bc050c21fa0a0da2d8b53f736c400a0087
adityarama1210 <=adityarama1210@github.com>
2016-05-15 19:35:03 +0700

    simple and basic map added

--- betterpet/engine/resources/views/home/index.blade.php
+++ betterpet/engine/resources/views/home/index.blade.php
@@ -193,8 +193,25 @@
   </div>
 </div>
 
-
-
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div id="googleMap" style="width:500px;height:380px;"></div>
+        </div>
+    </div>
+</div>
 
 <!-- Script to Activate the Carousel -->
 <script>
