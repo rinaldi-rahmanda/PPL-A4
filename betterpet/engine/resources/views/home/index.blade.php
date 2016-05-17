77e3bd6705492234d90bde3370c9f2d003c630f2
lianygustina <lianygustina@gmail.com>
2016-05-17 20:13:06 +0700

    gj

--- betterpet/engine/resources/views/home/index.blade.php
+++ betterpet/engine/resources/views/home/index.blade.php
@@ -3,25 +3,25 @@
 @section('content')
 <script>
     $(document).ready(function(){
-      if(!($("#flsmsg").html()==="")){
-        $("#flash-msg").delay(1500).fadeOut("slow");        
-      }
-      else{
-        $("#flash-msg").hide(); 
-      }
+        if(!($("#flsmsg").html()==="")){
+            $("#flash-msg").delay(1500).fadeOut("slow");        
+        }
+        else{
+            $("#flash-msg").hide(); 
+        }
     });    
 </script>
 <!-- Full Page Image Background Carousel Header -->
 <div id="flash-msg" class="center-block"
-    style="background-color:pink;position:absolute
-        ;margin-top:100px;width:100%;z-index:999;width:300px;border-radius: 10px;
-        margin-left:40%;">
+     style="background-color:pink;position:absolute
+            ;margin-top:100px;width:100%;z-index:999;width:300px;border-radius: 10px;
+            margin-left:40%;">
     @if(Session::get('msg'))
     <h4 class="text-center" id="flsmsg">{{Session::get('msg')}}</h4>
     @else
     <h4 class="text-center" id="flsmsg"></h4>
     @endif
-    
+
 </div>
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
@@ -101,7 +101,7 @@
                     </h5>
                 </blockquote>
             </div>
-            
+
         </div>
     </div>  
 </div>
@@ -135,62 +135,61 @@
         @if(count($adoptions)>0)
         <div class="row">
             <div class="col-md-offset-1 col-xs-12 col-sm-6 col-md-10">
-            @foreach ($adoptions as $adoption)
-            <div class="shelter-adoption col-md-4 col-sm-12 col-xs-12">
-                <img class="img-responsive img-rounded img-adoption" width="300px" height="300px" src="{{URL::to('/engine/storage/app/adoptionimage')}}/{{$adoption->picture}}">
-                <h3 class="text-center">{{$adoption->name}}</h3>
-                <p class="text-center"><a href="{{URL::to('/adoption')}}/{{$adoption->id}}" class="btn btn-primary" role="button">See the details</a></p>
-            </div>
-            @endforeach
+                @foreach ($adoptions as $adoption)
+                <div class="shelter-adoption col-md-4 col-sm-12 col-xs-12">
+                    <img class="img-responsive img-rounded img-adoption" width="300px" height="300px" src="{{URL::to('/engine/storage/app/adoptionimage')}}/{{$adoption->picture}}">
+                    <h3 class="text-center">{{$adoption->name}}</h3>
+                    <p class="text-center"><a href="{{URL::to('/adoption')}}/{{$adoption->id}}" class="btn btn-primary" role="button">See the details</a></p>
+                </div>
+                @endforeach
             </div>
         </div>
         @else
         There are no adoption available right now
         @endif
 
-            
-        </div><!-- End row -->
 
-    </div><!-- End container -->
+    </div><!-- End row -->
 
-</div>
+</div><!-- End container -->
+
 
 
 
 <div class="container-fluid text-center" style="min-height:300px;">
-  
-  <h2 style="margin-top:80px;">What our customers say</h2>
-  <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators1 carousel-indicators" style="bottom:-100px;">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <h4>"This company is the best. I am so happy with the result!"<br><span style="font-style:normal;">Michael Roe, Vice President, Comment Box</span></h4>
-      </div>
-      <div class="item">
-        <h4>"One word... WOW!!"<br><span style="font-style:normal;">John Doe, Salesman, Rep Inc</span></h4>
-      </div>
-      <div class="item">
-        <h4>"Could I... BE any more happy with this company?"<br><span style="font-style:normal;">Chandler Bing, Actor, FriendsAlot</span></h4>
-      </div>
+    <h2 style="margin-top:80px;">What our customers say</h2>
+    <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators1 carousel-indicators" style="bottom:-100px;">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <h4>"This company is the best. I am so happy with the result!"<br><span style="font-style:normal;">Michael Roe, Vice President, Comment Box</span></h4>
+            </div>
+            <div class="item">
+                <h4>"One word... WOW!!"<br><span style="font-style:normal;">John Doe, Salesman, Rep Inc</span></h4>
+            </div>
+            <div class="item">
+                <h4>"Could I... BE any more happy with this company?"<br><span style="font-style:normal;">Chandler Bing, Actor, FriendsAlot</span></h4>
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control1 carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control1 carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control1 carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control1 carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
 </div>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script><script>
 function init() {
