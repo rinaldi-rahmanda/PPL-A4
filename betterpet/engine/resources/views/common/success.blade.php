8661ab6aca765dff4c55c7ef6f5aa861cdea1ec1
adityarama1210 <=adityarama1210@github.com>
2016-05-25 23:32:58 +0700

    warning message

--- betterpet/engine/resources/views/common/success.blade.php
+++ betterpet/engine/resources/views/common/success.blade.php
@@ -1,6 +1,6 @@
 @if (session('success'))
     <!-- Form Error List -->
-    <div class="alert alert-info">
+    <div class="alert alert-warning">
         <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
         <strong>{{session('success')}}</strong>
     </div>
