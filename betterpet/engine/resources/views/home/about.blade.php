f17c341c18140e3077135cb835f531e18414e4ca
lianygustina <lianygustina@gmail.com>
2016-05-26 02:24:11 +0700

    benerin page contact dan about

--- betterpet/engine/resources/views/home/about.blade.php
+++ betterpet/engine/resources/views/home/about.blade.php
@@ -7,7 +7,7 @@
             <div class="col-md-12 col-xs-12">
                 <h1 class="text-center font-header"> About us</h1>
                 <hr class="line">
-                <div class="desc text-center">
+                <div class="col-md-offset-1 col-md-10 col-sm-12 text-center">
                     <h4>Tired of searching through Facebook and Twitter to find dogs or cat for adoption? You have come to the right place! With BetterPet, you can find dogs and cats ready for adoption, or advertise your own animals for adoption. As well, you can connect them with a veterinarian, animal care centers and pet supply stores. Here, you can also share information and stories about the animals in a forum that we provide. So, what are you waiting for? Join BetterPet today!
                     </h4>
 
@@ -38,12 +38,12 @@
         </div>
     </div>
     <div class="row">
-        <div class="team">
+        <div class="team hidden-xs">
             <h1 class="text-center font-header">Meet the team</h1>
             <hr class="line">
             <div class="team">
                 <div class="row team-row" style="margin-top: 6%;" >
-                    <div class="col-xs-3  col-md-2 col-md-offset-3">
+                    <div class="col-xs-3  col-md-2 col-md-offset-3 col-sm-offset-2">
                         <img class="img-responsive img-circle" src="{{URL::to('/image/adit.jpg')}}" width="150" height="150">
                     </div>
                     <div class="col-md-4 col-xs-6">
@@ -56,56 +56,134 @@
                 </div>
                 <hr class="line2">
                 <div class="row team-row">
-                    <div class="col-xs-3  col-md-4 col-md-offset-3">
+                    <div class="col-xs-6  col-md-4 col-md-offset-3 col-sm-offset-2">
                         <h3>Faiz Kautsar</h3>
                         <blockquote><i>
 
-                           "I really love cat, and i think betterpet will really helpful for pet lovers."
+                            "I really love cat, and i think betterpet will really helpful for pet lovers."
 
                             </i>
                         </blockquote>
                     </div>
-                    <div class="col-md-2 col-xs-4 img-team">
+                    <div class="col-md-2 col-xs-4">
                         <img class="img-responsive img-circle" src="{{URL::to('/image/faiz.jpg')}}" width="150" height="150">
 
                     </div> 
                 </div>
                 <hr class="line2">
                 <div class="row team-row">
-                    <div class="col-xs-3  col-md-2 col-md-offset-3">
+                    <div class="col-xs-3  col-md-2 col-md-offset-3 col-sm-offset-2">
                         <img class="img-responsive img-circle" src="{{URL::to('/image/me.jpg')}}" width="150" height="150">
                     </div>
                     <div class="col-md-4 col-xs-6">
                         <h3>Liany Gustina</h3>
                         <blockquote>
                             <i>
-                            "I hope that BetterPet will help people to find their dream pet."</i>
+                                "I hope that BetterPet will help people to find their dream pet."</i>
                         </blockquote>
                     </div> 
                 </div>
                 <hr class="line2">
                 <div class="row team-row">
-                    <div class="col-xs-3 col-md-4 col-md-offset-3">
+                    <div class="col-xs-6 col-md-4 col-md-offset-3 col-sm-offset-2">
                         <h3>Rinaldi Andrian Rahmanda</h3>
                         <blockquote><i>
                             "What greater gift than the love of a cat." ~Charles Dickens
                             </i>
                         </blockquote>
                     </div>
-                    <div class="col-md-2 col-xs-4 img-team">
+                    <div class="col-md-2 col-xs-4">
                         <img class="img-responsive img-circle" src="{{URL::to('/image/aldi.jpg')}}" width="150" height="150">
 
                     </div> 
                 </div>
                 <hr class="line2">
                 <div class="row team-row" style="margin-bottom:4%;">
-                    <div class="col-xs-3 col-md-2 col-md-offset-3">
+                    <div class="col-xs-3 col-md-2 col-md-offset-3 col-sm-offset-2">
                         <img class="img-responsive img-circle" src="{{URL::to('/image/tommy.jpg')}}" width="150" height="150">
                     </div>
                     <div class="col-md-4 col-xs-4" >
                         <h3>Tommy Saprikan Kahar</h3>
                         <blockquote><i>
-                            Outside of a pet, a book is man's best friend. Inside of a pet it's too dark to read
+                            "Outside of a pet, a book is man's best friend. Inside of a pet it's too dark to read"
+                            </i>
+                        </blockquote>
+                    </div> 
+                </div>
+
+            </div>
+        </div>   
+
+        <div class="team hidden-sm hidden-md hidden-lg">
+            <h1 class="text-center font-header">Meet the team</h1>
+            <hr class="line">
+            <div class="team">
+                <div class="row team-row" style="margin-top: 6%;" >
+                    <div class="col-xs-offset-1 col-xs-11 ">
+                        <img class="img-responsive img-circle" src="{{URL::to('/image/adit.jpg')}}" width="150" height="150">
+                    </div>
+                    <div class="col-xs-offset-1 col-xs-11">
+                        <h3>Aditya Rama</h3>
+                        <blockquote><i>
+                            "Growing together with a pet is wonderful, and i hope i'm not the only one who feel it."
+                            </i>
+                        </blockquote>
+                    </div> 
+                </div>
+                <hr class="line2">
+                <div class="row team-row">
+                    <div class="col-xs-offset-1 col-xs-11">
+                        <img class="img-responsive img-circle" src="{{URL::to('/image/faiz.jpg')}}" width="150" height="150">
+
+                    </div> 
+                    <div class="col-xs-offset-1 col-xs-11">
+                        <h3>Faiz Kautsar</h3>
+                        <blockquote><i>
+
+                            "I really love cat, and i think betterpet will really helpful for pet lovers."
+
+                            </i>
+                        </blockquote>
+                    </div>
+
+                </div>
+                <hr class="line2">
+                <div class="row team-row">
+                    <div class="col-xs-offset-1 col-xs-11">
+                        <img class="img-responsive img-circle" src="{{URL::to('/image/me.jpg')}}" width="150" height="150">
+                    </div>
+                    <div class="col-xs-offset-1 col-xs-11">
+                        <h3>Liany Gustina</h3>
+                        <blockquote>
+                            <i>
+                                "I hope that BetterPet will help people to find their dream pet."</i>
+                        </blockquote>
+                    </div> 
+                </div>
+                <hr class="line2">
+                <div class="row team-row">
+                    <div class="col-xs-offset-1 col-xs-11">
+                        <img class="img-responsive img-circle" src="{{URL::to('/image/aldi.jpg')}}" width="150" height="150">
+
+                    </div> 
+                    <div class="col-xs-offset-1 col-xs-11">
+                        <h3>Rinaldi Andrian Rahmanda</h3>
+                        <blockquote><i>
+                            "What greater gift than the love of a cat." ~Charles Dickens
+                            </i>
+                        </blockquote>
+                    </div>
+
+                </div>
+                <hr class="line2">
+                <div class="row team-row" style="margin-bottom:4%;">
+                    <div class="col-xs-offset-1 col-xs-11">
+                        <img class="img-responsive img-circle" src="{{URL::to('/image/tommy.jpg')}}" width="150" height="150">
+                    </div>
+                    <div class="col-xs-offset-1 col-xs-11" >
+                        <h3>Tommy Saprikan Kahar</h3>
+                        <blockquote><i>
+                            "Outside of a pet, a book is man's best friend. Inside of a pet it's too dark to read"
                             </i>
                         </blockquote>
                     </div> 
--- betterpet/engine/resources/views/home/contact.blade.php
+++ betterpet/engine/resources/views/home/contact.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 	<div class='container-fluid page-wrap'>
 		<div class="row">
-			<div class="col-md-6 col-xs-6 leftHalf">
+			<div class="col-md-6 hidden-xs leftHalf">
 			</div>
-			<div class="col-md-6 col-xs-6 rightHalf">
+			<div class="col-md-6 col-xs-6 hidden-xs rightHalf">
 				@if (count($errors) > 0)
 				    <!-- Form Error List -->
 				    <div class="alert alert-danger">
@@ -30,9 +30,53 @@
 				@endif
 				
 				</div>
-	
-    
 				
+				
+                <h3 class='text-center'>Ask us anything!</h3>
+                <br>
+    				<form method="POST">
+                    {!! csrf_field() !!}
+                    <div class="form-group">
+					<input type="text" class="register-form form-control" name="name" placeholder="Your Name (20 character maximum)" value="{{old('name')}}" required></div>
+					<div class="form-group">
+					<input type="text" class="register-form form-control" name="email" placeholder="Your Email Address" required value="{{old('email')}}"></div>
+					<div class="form-group">
+					<input type="text" class="register-form form-control" name="title" value="{{old('title')}}" placeholder="Title" required></div>
+					<div class="form-group">
+					<textarea name="content" class="register-form form-control" placeholder="What are you wondering?" required>{{old('content')}}</textarea></div>
+					<div class="form-group">
+					
+					    <div class="g-recaptcha" data-sitekey="6LcFFR0TAAAAAPQzHzv5P_PgB00WTI_I-GaFxE_P"></div>
+					<button class='btn btn-primary pull-right'>Send</button></div>
+				</form>
+			</div>
+            
+            
+            <div class="col-md-6 col-xs-12 hidden-sm hidden-md hidden-lg col-xs-12" style="margin-top:20%;">
+				@if (count($errors) > 0)
+				    <!-- Form Error List -->
+				    <div class="alert alert-danger">
+				        <strong>Failed to send the question :(</strong>
+
+				        <br><br>
+
+				        <ul>
+				            @foreach ($errors->all() as $error)
+				                <li>{{ $error }}</li>
+				            @endforeach
+				        </ul>
+				    </div>  
+				@endif
+				
+				<div id="flash-msg" class="center-block"
+				style="background-color:pink;position:absolute
+				;margin-top:435px;width:100%;z-index:999;width:300px;border-radius: 10px;
+				">
+				@if(session('success'))
+					    <h4 class="text-center" id="flsmsg">{{Session('success')}}</h4>
+				@endif
+				
+				</div>
 				
 				
                 <h3 class='text-center'>Ask us anything!</h3>
